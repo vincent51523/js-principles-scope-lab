69687b3ab54c26573cde0292eaa5e550abf13255
bin <vincent418@icloud.com>
2021-04-05 02:49:57 -0500

    commit tree 7a6f23f9ac3e23b127c96ecc7ef0418a88f54890

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+ var customerName = "bob";
+// function customerName() {
+//     var customerName = "bob";
+//     return customerName;
+// }
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+ 
+function setBestCustomer() {
+     bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+     bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "bob";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob";
+}
+
+
+
